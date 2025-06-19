964de11f9f784dde41362dddbc7a679671202e5e
elsonmorina <167892030+elsonmorina@users.noreply.github.com>
2025-06-19 07:05:32 -0700

    day6

---
+++ front-page.php
@@ -0,0 +1,16 @@
+<?php het_header(); ?>
+<div id="container">
+    <div class="inner_container">
+        <div class="imgs_container">
+            <div class="img_container">
+                <img src="<?php echo get_template_directory_uri(); ?> /images/1.jpg" alt="">
+            </div>
+            <div class="img_container">
+                <img src="<?php echo get_template_directory_uri(); ?> /images/2.jpg" alt="">
+            </div>
+            <div class="img_container">
+                <img src="<?php echo get_template_directory_uri(); ?> /images/3.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</div>
--- functions.php
+++ functions.php
@@ -47,4 +47,52 @@ function config(){
 
 add_action('after_setup_theme','config',0);
 
+    function wp_devs_sidebars(){
+        register_sidebar(
+            array(
+                'name'=>'Blog Sidebar',
+                'id'=>'siderbar-blog',
+                'description'=>'This is the Blog Sidebar. You can add your widgets here',
+                'before_widget'=>'<div class="widget-wrapper">',
+                'after_widget'=>'</div>',
+                'before_title'=>'<h4 class="widget-title">',
+                'after_title'=>'</h4>'
+            )
+        );
+        register_sidebar(
+            array(
+                'name'=>'Service 1',
+                'id'=>'services-1',
+                'description'=>'First Service Area',
+                'before_widget'=>'<div class="widget-wrapper">',
+                'after_widget'=>'</div>',
+                'before_title'=>'<h4 class="widget-title">',
+                'after_title'=>'</h4>'
+            )
+        );
+        register_sidebar(
+            array(
+                'name'=>'Service 2',
+                'id'=>'services-2',
+                'description'=>'Second Service Area',
+                'before_widget'=>'<div class="widget-wrapper">',
+                'after_widget'=>'</div>',
+                'before_title'=>'<h4 class="widget-title">',
+                'after_title'=>'</h4>'
+            )
+        );
+        register_sidebar(
+            array(
+                'name'=>'Service 3',
+                'id'=>'services-3',
+                'description'=>'Third Service Area',
+                'before_widget'=>'<div class="widget-wrapper">',
+                'after_widget'=>'</div>',
+                'before_title'=>'<h4 class="widget-title">',
+                'after_title'=>'</h4>'
+            )
+        );
+    }
+
+    add_action('widgets_init','wp_devs_sidebars');
 ?>
